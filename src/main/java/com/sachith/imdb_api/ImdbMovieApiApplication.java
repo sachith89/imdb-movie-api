3298b0f2dbf7bd8991d73e9a65661173d0b67e84
Sachith <sachithlakmal@icloud.com>
2022-07-30 14:02:41 +0530

    initial commit

---
+++ src/main/java/com/sachith/imdb_api/ImdbMovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.sachith.imdb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbMovieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImdbMovieApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sachith/imdb_api/ImdbMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachith.imdb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbMovieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
